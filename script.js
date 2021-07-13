55026ac5259e01f965211a7c2de75422857020ed
Aditya Salunke <adisalu15@gmail.com>
2021-07-13 23:57:58 +0530

    add cahnges to script.js and created helper function to randomize and  check option and determines draw or win

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("HI, i am Aditya")
+
+
+function computerPlay(){
+    var option =
+    ["rock","paper","scissor"];
+    var a =Math.floor(Math.random()*option.length);
+    
+    return option[a];
+}
+
+function versus(playerSelection){
+        a = playerSelection;
+        console.log(computerPlay);
+        let cSelection = computerPlay;
+        
+        if(a == cSelection){
+            console.log("Draw");
+        }
+        else{
+            console.log('you win');
+        }
+    }
