dd62f6dbb664fe943c0da76f46639545b7a673e7
Aditya Salunke <adisalu15@gmail.com>
2021-07-14 19:20:28 +0530

    changes to script file , determines if the user win , loses or draw for all circumstances

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 
-
-function computerPlay(){
+let a = "";
+function computerPlay() {
     var option =
-    ["rock","paper","scissor"];
-    var a =Math.floor(Math.random()*option.length);
-    
+        ["rock", "paper", "scissor"];
+    var a = Math.floor(Math.random() * option.length);
+
+
     return option[a];
 }
 
-function versus(playerSelection){
-        a = playerSelection;
-        console.log(computerPlay);
-        let cSelection = computerPlay;
-        
-        if(a == cSelection){
-            console.log("Draw");
-        }
-        else{
-            console.log('you win');
-        }
+function versus(playerSelection, computerSelection) {
+    a = playerSelection;
+
+
+    let pSelected = a.toLowerCase();
+
+    let cSelection = computerSelection();
+
+        console.log(cSelection);
+    if (pSelected == cSelection) {
+        console.log("Draw");
     }
+    else if (
+        (pSelected == 'rock' && cSelection == 'scissor') ||
+        (pSelected == 'paper' && cSelection == 'rock') ||
+        (pSelected == 'scissor' && cSelection == 'paper')
+    ) {
+        console.log('you won');
+    }
+    else{
+        console.log('you lose');
+    }
+}
